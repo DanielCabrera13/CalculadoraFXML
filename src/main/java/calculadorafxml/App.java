7bd06222cf34d5dbd7b5e6094057ac4bcbf0e0fe
DanielCabrera13 <danielcabreracabrera13@gmail.com>
2022-01-16 20:24:38 +0000

    Update

--- src/main/java/calculadorafxml/App.java
+++ src/main/java/calculadorafxml/App.java
@@ -2,21 +2,23 @@ package calculadorafxml;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
 	
 	private CalculadoraController calculadoraController;
+	private static Scene scene;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
 		calculadoraController = new CalculadoraController();
 		
-		Scene scene = new Scene(calculadoraController.getView());
-		scene.getStylesheets().add("/css/modern.css");
-
+		
+		scene = new Scene(calculadoraController.getView());
 		
 		primaryStage.setTitle("Calculadora");
 		primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("../calculator-32x32.png")));
@@ -28,6 +30,10 @@ public class App extends Application {
 	public static void main(String[] args) {
 		launch(args);
 
+	}
+	
+	public static Scene getScene() {
+		return scene;
 	}
 
 }
